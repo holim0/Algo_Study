3902b8028282d760cd30d28e2cdb9cd20ce775e3
이희제 <holim1226@gmail.com>
2020-08-24 22:57:10 +0900

    2020-08-24

---
+++ 10953.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int a, b;
+    int t;
+    char val;
+
+    cin>>t;
+
+    for (int i = 0; i < t; i++)
+    {
+        cin>>a; cin>>val>>b;
+
+        cout<<a+b<<endl;
+    }
+    
+
+
+    return 0;
+}
