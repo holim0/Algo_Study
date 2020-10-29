ce8e389235e9910f5c2adcf5ae8152b324501662
이희제 <holim1226@gmail.com>
2020-10-29 19:53:54 +0900

    2020-10-29

---
+++ 점프와 순간이동.py
@@ -0,0 +1,17 @@
+def solution(n):
+    ans = 0
+
+    while True:
+
+        if n == 1:
+            ans += 1
+            break
+
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n -= 1
+            n /= 2
+            ans += 1
+
+    return ans
