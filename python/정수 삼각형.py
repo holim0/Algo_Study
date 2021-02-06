0f23903f630f31ccdf10197857b2a3e1269a06ff
이희제 <holim1226@gmail.com>
2021-02-06 14:09:11 +0900

    2021-02-06

---
+++ python/정수 삼각형.py
@@ -0,0 +1,17 @@
+def solution(triangle):
+    answer = 0
+    size = len(triangle)
+
+    for i in range(1, size):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+
+            elif j == len(triangle[i])-1:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+
+    answer = max(triangle[-1])
+
+    return answer
