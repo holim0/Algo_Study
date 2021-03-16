180a7f9180cf76a9b76bb33f1cc36cadc3809ef6
이희제 <holim1226@gmail.com>
2021-03-16 23:45:08 +0900

    2021-03-16

---
+++ python/leet#344.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+        for i in range(len(s)//2):
+            tmp = s[i]
+            s[i] = s[-i-1]
+            s[-i-1] = tmp
