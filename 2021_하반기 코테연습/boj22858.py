b5ce9e488e5ca284b44956dabbdc1a5618bb27d1
이희제 <holim1226@gmail.com>
2021-10-05 16:47:19 +0900

    2021-10-05

--- 2021_하반기 코테연습/boj22858.py
+++ 2021_하반기 코테연습/boj22858.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-import copy
+
 n, k = map(int, input().split())
 
 answer = []
@@ -8,20 +8,15 @@ after_k = list(map(int, input().split()))
 
 d = list(map(int, input().split()))
 
-change_dic = defaultdict(int)
-
-
-for i in range(len(d)):
-    change_dic[i] = d[i]-1 ## i를 d[i]-1로
 
 
 
 for _ in range(k):
-    tmp = [0 for _ in range(n)]
+    tmp = [0] *n
     for i in range(n):
-        tmp[change_dic[i]] = after_k[i] 
+        tmp[d[i]-1] = after_k[i] 
 
-    after_k = copy.deepcopy(tmp)
+    after_k = tmp
 
 
 
