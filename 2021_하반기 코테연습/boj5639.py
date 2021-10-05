9e971b63ed3aad0a23a6356603365c13f705c016
이희제 <holim1226@gmail.com>
2021-10-05 22:59:17 +0900

    2021-10-05

---
+++ 2021_하반기 코테연습/boj5639.py
@@ -0,0 +1,40 @@
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+number = []
+
+
+            
+
+
+
+def postOrder(s, e):
+   
+    if s>e:
+        return 
+    else:
+        root = number[s]
+        divide = e+1
+        for i in range(s+1, e+1):
+            if number[i]>root:
+                divide = i
+                break
+        postOrder(s+1, divide-1)
+        postOrder(divide, e)
+        print(root)
+
+
+while 1:
+  try:
+    number.append(int(input()))
+  except:
+    break
+
+start, end = 0, len(number)-1
+postOrder(start, end)
+
+
+
+
+
+
