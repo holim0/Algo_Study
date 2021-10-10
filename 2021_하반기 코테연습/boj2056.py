e41aca1cb34610af063ffb1fcbb05de8c65eeb61
이희제 <holim1226@gmail.com>
2021-10-10 15:13:21 +0900

    2021-10-10

---
+++ 2021_하반기 코테연습/boj2056.py
@@ -0,0 +1,51 @@
+from collections import deque
+
+n = int(input())
+
+answer = 0
+
+degree = [0 for _ in range(n+1)]
+
+link = [[] for _ in range(n+1)]
+
+need_time = [0 for _ in range(n+1)]
+
+dp = [0 for _ in range(n+1)]
+
+for i in range(1, n+1):
+    info = list(map(int, input().split()))
+
+    degree[i]+=info[1]
+    need_time[i] = info[0]
+
+    for j in range(2, len(info)):
+        cur = info[j]
+        link[cur].append(i)
+
+q =deque([])
+
+
+for i in range(1, n+1):
+    if degree[i]==0:
+        dp[i] = need_time[i]
+        q.append((i, need_time[i]))
+
+
+
+while q:
+    cur, time = q.popleft()
+
+   
+    for nxt in link[cur]:
+        degree[nxt]-=1
+        dp[nxt] = max(dp[nxt], dp[cur]+need_time[nxt])
+        if degree[nxt]==0:
+            q.append((nxt, need_time[nxt]))
+
+print(dp)
+for d in dp:
+    answer = max(answer, d)
+    
+    
+print(answer)
+
